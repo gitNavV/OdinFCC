99fc2f0f57ad6e4e8653e876e83d2628dd91cd62
gitNavV <navneetdas53@gmail.com>
2018-01-24 10:31:39 +0530

    Completed node web server

--- NodeWebServer/printMessages.js
+++
@@ -1,7 +0,0 @@
-const messageModule = require('./messages'); 1
-
-messageModule.messages.forEach( (m) =&gt; { 2
-
-  console.log(m);
-
-});
--- NodeWebServer/zip_connection/messages.js
+++ NodeWebServer/zip_connection/messages.js
@@ -1,1 +1,2 @@
+let messages = ["You are great!", "You can accomplish anything!", "Success is in your future!"];
 exports.messages =["You are great!", "You can accomplish anything!", "Success is in your future!"];
