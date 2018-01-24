537c7c6b8dfb5179c7ff829c3110bf5587f2db65
gitNavV <navneetdas53@gmail.com>
2018-01-24 10:40:14 +0530

    Modified main.js

--- NodeWebServer/simple_server/main.js
+++ NodeWebServer/simple_server/main.js
@@ -7,7 +7,7 @@ const app = http.createServer((request, response) => {
   if (query.zipCode) city = cities.zip_lookup(query.zipCode).city;
   else city = "not found"
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write('<h1>The city you are in is ${city}.</h1>');
+  response.write(`<h1>The city you are in is ${city}.</h1>`);
   response.end();
 });
 
